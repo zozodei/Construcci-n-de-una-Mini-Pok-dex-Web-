63a860c427d272e27068c5860537ce6b5a6b9f8d
Manuel <manuelmandel09@gmail.com>
2026-04-13 08:27:52 -0300

    terminado

--- script.js
+++ script.js
@@ -1,27 +1,50 @@
-/*<input id="search-input"
-           type="text"
-           placeholder="pikachu, charizard, 25…" />
-    <button id="search-btn">BUSCAR</button>*/
 async function buscarPokemon() {
-    const nombre = document.getElementById("buscarInput").value.toLowerCase();
-    const resultado = document.getElementById("resultado");
-
+    let nombre = document.getElementById("buscarInput").value.trim();
+    const loader = document.getElementById("pokeball");
+    const buscando = document.getElementById("loader-text");
+    const errorDiv = document.getElementById("error-msg");
     if (!nombre) {
-        /*ERRORRRRRRRRRR*/
+        alert("Input incorrecto");
         return;
     }
 
-    const respuesta = await fetch(`https://pokeapi.co/api/v2/pokemon/${nombre}`);
+    nombre = nombre.toLowerCase();
+    const resultado = document.getElementById("resultado");
 
-    const data = await respuesta.json();
 
-    document.getElementById("nombre").textContent = data.name.toUpperCase();
-    document.getElementById("imagen").src = data.sprites.front_default;
-    const tipos = data.types.map(e => e.type.name).join(", ");
-    document.getElementById("tipo").textContent = tipos;
+    try {
+        const respuesta = await fetch(`https://pokeapi.co/api/v2/pokemon/${nombre}`);
 
-    document.getElementById("peso").textContent = data.weight;
-    document.getElementById("altura").textContent = data.height;
+        if (!respuesta.ok) {
+            throw new Error("Pokémon no encontrado");
+        }
 
-    resultado.hidden = false;
+        errorDiv.hidden = true;
+        loader.hidden = true;
+        buscando.hidden = true;
+        
+        const data = await respuesta.json();
+
+        document.getElementById("nombre").textContent = data.name.toUpperCase();
+        document.getElementById("imagen").src = data.sprites.front_default;
+        const tipos = data.types.map(e => e.type.name).join(", ");
+        document.getElementById("tipo").textContent = tipos;
+
+        document.getElementById("peso").textContent = data.weight;
+        document.getElementById("altura").textContent = data.height;
+
+        resultado.hidden = false;
+
+    } catch (error) {
+
+        resultado.hidden = true;
+
+        loader.hidden = false;
+
+        buscando.hidden = false;
+        
+        errorDiv.hidden = false;
+
+        document.getElementById("error-text").textContent = error.message;
+    }
 }
